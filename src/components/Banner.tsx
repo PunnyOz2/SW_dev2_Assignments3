edde19dc6133a424f24ec53acb19e2216ff06578
Tawatwee Aiemarreerat <77015953+PunnyOz2@users.noreply.github.com>
2023-08-23 23:17:42 +0700

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Banner() {
   return (
     <div className={styles.banner}>
-      <Image src="/img/boosterdose.jpg" alt="Booster Dose" layout="fill" objectFit="cover" priority/>
+      <Image src="/nextjs-github-pages/img/boosterdose.jpg" alt="Booster Dose" layout="fill" objectFit="cover" priority/>
       <div className={styles.bannerText}>
         <h1>Covid-19 Booster Dose</h1>
         <h2>Get your booster dose today!</h2>
